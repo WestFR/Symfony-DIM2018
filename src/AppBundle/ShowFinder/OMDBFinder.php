af41e31f1ab101fc384aaa3765a44d53a522c2a5
= <=>
2018-02-19 14:07:36 +0100

    Add ApiKey of OMDB API in parameters.yml

--- src/AppBundle/ShowFinder/OMDBFinder.php
+++ src/AppBundle/ShowFinder/OMDBFinder.php
@@ -2,19 +2,58 @@
 
 namespace AppBundle\ShowFinder;
 
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Show;
 use GuzzleHttp\Client;
 
 class OMDBFinder implements ShowFinderInterface {
 
 	private $client;
+	private $apiKey;
 
-	public function __construct(Client $client) {
+	public function __construct(Client $client, $apiKey) {
 		$this->client = $client;
+		$this->apiKey = $apiKey;
 	}
 
 	public function findByName($query) {
-		$result = $this->client->get('/?apikey=b99f30d1&type=series&t='. '%' . $query . '%');
-        return json_decode($result->getBody());
+		$results = $this->client->get('/?apikey='.$this->apiKey.'&type=series&t="'.$query.'"');
+		$json = \GuzzleHttp\json_decode($results->getBody(), true);
+
+		if ($json['Response'] == 'False' && $json['Error'] == 'Series not found!') {
+			return [];
+		}
+ 
+		return json_decode($results->getBody());
+	}
+
+	/**
+	 * Create a private function that transform a OMDB JSON into a Show and Category
+	 *
+	 * @param String $json
+	 *
+	 * Show[] $shows
+	 */ 
+	private function convertToShow($json)
+	{
+		$category = new Category();
+		$category->setName($json['Genre']);
+
+		$shows = [];
+		$show = new Show();
+		$show
+			->setName($json['Title'])
+			->setDataSource(Show::DATA_SOURCE_OMDB)
+			->setAbstract('Not provided.')
+			->setCountry($json['Country'])
+			->setAuthor('TO DO when the authentication!')
+			->setReleaseDate(new \DateTime($json['Released']))
+			->setMainPicture($json['Poster'])
+			->setCategory($category);
+
+		$shows[] = $show;
+
+		return $shows;
 	}
 
 	public function getName() {
--- src/AppBundle/ShowFinder/ShowFinder.php
+++ src/AppBundle/ShowFinder/ShowFinder.php
@@ -9,11 +9,15 @@ class ShowFinder {
 	public function findByName($query) {
 
 		$tmp = [];
+		$results = [];
 
 		foreach ($this->finders as $finder) {
 			$tmp[$finder->getName()] = $finder->findByName($query);
+			//$results = array_merge($results, $finder->findByName($query));
 		}
+
 		return $tmp;
+		//return $results;
 	}
 
 	public function addFinder(ShowFinderInterface $finder) {
