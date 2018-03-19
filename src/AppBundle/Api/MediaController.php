f397b61eec2a7455705d45ef21fe1c4fda66d1a2
= <=>
2018-03-19 10:32:49 +0100

    Upload with MediaController

--- src/AppBundle/Api/MediaController.php
+++ src/AppBundle/Api/MediaController.php
@@ -12,6 +12,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Routing\Generator\RoutingGeneratorInterface;
+
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**	
@@ -23,24 +26,27 @@ class MediaController extends Controller {
 	 * @Method({"POST"})
 	 * @Route("/", name="_upload")
 	 */
-	public function uploadAction(Request $request) {
+	public function uploadAction(Request $request, FileUploader $fileUploader, RouterInterface $router) {
 
 		$media = new Media();
-		$form = $this->createForm(MediaType::class, $media);
+		$media->setFile($request->files->get('file')); 
 
-		$form->handleRequest($request);
+		// Validate media object
 
-		if($form->isValid()) {
+		$generatedFileName = $fileUploader->upload($media->getFile(), time());
+		
+		$path = $this->container->getParameter('upload_directory_file').'/'.$generatedFileName;
 
-			$generatedFileName = $fileUploader->upload($media->getFile(), time());
-			$path = $this->container->getParameter('upload_directory_file').$generatedFileName;
+		$baseUrl = $router->getContext()->getScheme().'://'.$router->getContext()->getHost().':'.$router->getContext()->getHttpPort();
+        $media->setPath($baseUrl.$path);
 
-            $media->setPath($request->getBaseUrl().$path);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($media);
+		$em->flush();
 
-        	return $this->returnResponse('Media uploaded', Response::HTTP_CREATED);
-		}
+        return $this->returnResponse('Media uploaded, path is : '.$media->getPath(), Response::HTTP_CREATED);
 
-		return $this->returnResponse('Media not uploaded', Response::HTTP_BAD_REQUEST);
+		//return $this->returnResponse('Media not uploaded', Response::HTTP_BAD_REQUEST);
 
 	}
 	
